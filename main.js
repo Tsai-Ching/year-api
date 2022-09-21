9a28e49c6fe4c5aaf5ecd93d18ff5112c0188141
Tsai-Ching <h93fu4@gmail.com>
2022-09-21 17:51:01 +0800

    uploading

---
+++ main.js
@@ -0,0 +1,18 @@
+let enter_btn = document.querySelector(".btn");
+let card = document.querySelector(".card");
+
+enter_btn.addEventListener("click", getFact);
+
+function getFact() {
+	let input = document.querySelector(".input").value;
+	let card_title = document.querySelector(".card-title");
+	let card_text = document.querySelector(".card-text");
+
+	fetch(`http://numbersapi.com/${input}/year`)
+	.then((response) => response.text())
+	.then((data) => {
+		card_title.innerHTML = input;
+		card_text.innerHTML = data;
+	});
+
+}
